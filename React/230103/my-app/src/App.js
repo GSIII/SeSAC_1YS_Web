17e3ea06461d9d07b0a27ea50e721177f0749c2a
GSIII <kalseo901@gmail.com>
2023-02-09 19:02:08 +0900

    feat:socket

--- React/230103/my-app/src/App.js
+++ React/230103/my-app/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 // import MainHeader from './components/MainHeader';
 import ImgComponent from './components/ImgComponent';
 import BtnToNaver from './components/BtnToNaver';
 
 function App() {
-
   return (
     <div className="App">
       <ImgComponent />
--- socket/230209/index.js
+++ socket/230209/index.js
@@ -7,30 +7,30 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-//{소켓아이디:닉네임, 소켓아이디:닉네임,...}
 let list = {};
 
 io.on("connection", (socket) => {
-  console.log("server socket connected");
+  console.log("Server Socket Connected");
 
   socket.emit("info", socket.id);
 
-  // io.emit("notice", socket.id + "님이 입장하셨습니다.");
+  //io.emit("notice", socket.id + "님이 입장하셨습니다.");
 
   socket.on("username", (name) => {
     list[socket.id] = name;
     io.emit("list", list);
     io.emit("notice", name + "님이 입장하셨습니다.");
   });
-
   socket.on("send", (json) => {
-    //json = {msg:~~~}
+    //json = {msg:~~~,to:~~~~}
     json["from"] = socket.id;
-    //json={msg:~~~~,from:~~~~~}
+    //json={msg:~~~~,from:~~~~~,to:~~~~}
     json["username"] = list[socket.id];
-    //json={msg:~~~~,from:~~~~,username:~~~}
+    //json={msg:~~~~,from:~~~~,username:~~~,to:~~~~}
+    json["is_dm"] = false;
     if (json.to === "전체") io.emit("newMSG", json);
     else {
+      json["is_dm"] = true;
       const socketID = Object.keys(list).find((key) => list[key] == json.to);
       io.to(socketID).emit("newMSG", json);
       socket.emit("newMSG", json);
@@ -44,6 +44,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(8080, () => {
-  console.log("server port : ", 8080);
+http.listen(8000, () => {
+  console.log("Server port : ", 8000);
 });
